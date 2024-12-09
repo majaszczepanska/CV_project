218d9f182c1c601e510028f17d064aab9d342613
majaszczepanska <139561097+majaszczepanska@users.noreply.github.com>
2024-12-09 23:18:19 +0100

    mail contact.php

--- contact.php
+++ contact.php
@@ -4,18 +4,22 @@ $lname = $_POST["lname"];
 $email = $_POST["email"];
 $message = $_POST["message"];
 
+
 if (empty($fname) || empty($lname) || empty($email) || empty($message)) {
     header("Location: index.html#contact");
     exit();
 } else {
-    echo "ok";
+    $to = "m.szczepanska.283@sltzn.katowice.pl"; 
+    $subject = "New message from $fname $lname";
+    $message_mail = "You have received a new message from your website contact form:\n\n" .
+                    "Name: $fname $lname\n" .
+                    "Email: $email\n\n" .
+                    "Message:\n$message";
+
+    if (mail($to, $subject, $message_mail)) {
+        echo "Thank you for your message. It has been sent successfully.";
+    } else {
+        echo "Sorry, something went wrong. Please try again later.";
+    }
 }
-
-
-
-
-
-
-
-
 ?>
